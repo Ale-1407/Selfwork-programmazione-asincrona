b1a224c7a825eb8a8e0e1508ad869ea26c2b9688
Ale-1407 <alessiacarrera07@gmail.com>
2025-11-13 09:01:54 +0100

    loader

--- render.js
+++ render.js
@@ -1,11 +1,35 @@
 import get_products from "./api.js";
 
+
+const showLoader = () => {
+    const loader = document.querySelector('.loader');
+    loader.classList.remove('d-none');
+};
+
+const hideLoader = () => {
+    const loader = document.querySelector('.loader');
+    loader.classList.add('d-none');
+};
+
+const wait = (ms = 0) => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve();
+        }, ms);
+    })
+};
+
+
 const create_card = async () =>{
+    showLoader();
     const products = await get_products();
+    
+    await wait(1000);
+
     products.forEach((el) => {
         const div = document.querySelector('.products-box');
         div.innerHTML += `
-            <div class="card m-1" style="width: 18rem;">
+            <div class="card m-2" style="width: 18rem;">
                 <div class="card-body">
                     <h5 class="card-name">${el.name}</h5>
                     <h6 class="card-category mb-2 text-body-secondary">Categoria: ${el.category}</h6>
@@ -13,9 +37,12 @@ const create_card = async () =>{
                 </div>
             </div>
         `;      
-
     });
+
+    hideLoader();
 }
 
 create_card();
+
+
 
